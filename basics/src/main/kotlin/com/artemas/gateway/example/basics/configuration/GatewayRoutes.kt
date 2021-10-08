1d7de3ff27670deb67b98bb0bcadd779cd7a61ac
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-10-08 09:26:19 +0100

    Amend route locator

--- basics/src/main/kotlin/com/artemas/gateway/example/basics/configuration/GatewayRoutes.kt
+++ basics/src/main/kotlin/com/artemas/gateway/example/basics/configuration/GatewayRoutes.kt
@@ -2,21 +2,20 @@ package com.artemas.gateway.example.basics.configuration
 
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
+import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class GatewayRoutes {
     @Bean
-    fun gateway(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator {
-        return routeLocatorBuilder
-            .routes()
-            .route { routeSpec ->
-                routeSpec
-                    .path("/hello")
-                    .filters { routeFilter -> routeFilter.setPath("/guides") }
-                    .uri("https://spring.io/")
+    fun gateway(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator =
+        routeLocatorBuilder.routes {
+            route {
+                path("/hello") // 👈 client requests this
+                filters { setPath("/guides") }
+                uri("https://spring.io/") // 👈 the downstream service that our service will access..
             }
-            .build()
-    }
+        }
 }
