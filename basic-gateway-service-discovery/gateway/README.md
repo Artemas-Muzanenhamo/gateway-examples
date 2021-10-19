f43a75fad66bf9bc5b345d1fbd46bb9839f36bde
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-10-19 07:13:39 +0100

    add gateway service

---
+++ basic-gateway-service-discovery/gateway/README.md
@@ -0,0 +1,4 @@
+# Gateway
+
+* This application is a single entry point for all clients.
+* Some requests are simply proxied/routed to the appropriate service. It handles other requests by fanning out to multiple services.
---
+++ basic-gateway-service-discovery/gateway/src/test/kotlin/com/artemas/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemas.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
